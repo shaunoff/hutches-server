2638b9e50d987aa6f8b47ea0661d61c4a39c5a14
Shaun Hutch <shaun@promoboxx.com>
2020-04-27 14:10:44 -0400

    linting

--- prettier.config.js
+++ prettier.config.js
@@ -1,1 +1,1 @@
-module.exports = require('tslint-config-sst/prettier.config')
+module.exports = {};
--- src/modules/user/resolvers/mutation.ts
+++ src/modules/user/resolvers/mutation.ts
@@ -1,18 +1,11 @@
-import { UserAPI } from '../providers'
-import { MutationResolvers } from '@models'
-//@ts-ignore
-const testFunc = (tetsy) => (next) => (root, args, context, info) => {
-  console.log('iytrureyiuyiuerytiuyeriutyeriuyteuriytu', context)
-  return next(root, args, context, info)
-}
+import { UserAPI } from "../providers";
+import { MutationResolvers } from "@models";
 export const Mutation: MutationResolvers = {
-  //@ts-ignore
   signup: (root, { data = {} }, { injector }) => {
-    //console.log(token)
-    return injector.get(UserAPI).signup(data)
+    return injector.get(UserAPI).signup(data);
   },
 
   signin: (root, { data }, { createToken, injector }) => {
-    return injector.get(UserAPI).signin(data, createToken)
-  },
-}
+    return injector.get(UserAPI).signin(data, createToken);
+  }
+};
