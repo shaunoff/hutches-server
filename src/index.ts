7ddafed3aa91a411984fcf9b40094d524c82388c
Shaun Hutch <shaun@promoboxx.com>
2020-04-27 21:53:05 -0400

    port

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,42 @@
-import 'reflect-metadata'
+import "reflect-metadata";
 import {
   createConnection,
   getRepository,
   In,
-  createQueryBuilder,
-} from 'typeorm'
-import { ApolloServer } from 'apollo-server'
-import { Modules } from './modules'
-import AppConfig from 'config/AppConfig'
-import databaseConfig from 'config/databaseConfig'
-import Users from './modules/user/models'
-import { userFromToken, createToken } from './auth'
-import directives from './directives'
-import { loadSchemaFiles } from 'graphql-toolkit'
-import { SchemaDirectiveVisitor } from 'graphql-tools'
+  createQueryBuilder
+} from "typeorm";
+import { ApolloServer } from "apollo-server";
+import { Modules } from "./modules";
+import AppConfig from "config/AppConfig";
+import databaseConfig from "config/databaseConfig";
+import Users from "./modules/user/models";
+import { userFromToken, createToken } from "./auth";
+import directives from "./directives";
+import { loadSchemaFiles } from "graphql-toolkit";
+import { SchemaDirectiveVisitor } from "graphql-tools";
 
 createConnection(databaseConfig)
-  .then(async (connection) => {
-    const { typeDefs, resolvers, schema, schemaDirectives } = Modules
-    SchemaDirectiveVisitor.visitSchemaDirectives(schema, schemaDirectives)
+  .then(async connection => {
+    const { typeDefs, resolvers, schema, schemaDirectives } = Modules;
+    SchemaDirectiveVisitor.visitSchemaDirectives(schema, schemaDirectives);
     const server = new ApolloServer({
       //typeDefs: loadSchemaFiles('./directives/directives.graphql'),
       typeDefs,
       resolvers,
       schema,
       context: async ({ req }) => {
-        const token = req.headers.authorization
-        const user = await userFromToken(token)
-        return { user, createToken }
+        const token = req.headers.authorization;
+        const user = await userFromToken(token);
+        return { user, createToken };
       },
       // Add Engine here for analytics
       tracing: AppConfig.TRACING,
-      debug: AppConfig.DEBUG,
+      debug: AppConfig.DEBUG
       //schemaDirectives: directives,
-    })
+    });
 
-    server.listen().then(({ url }) => {
-      console.log(`🚀  Server ready at ${url}`)
-    })
+    server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+      console.log(`🚀  Server ready at ${url}`);
+    });
   })
-  .catch((error) => console.log(error))
+  .catch(error => console.log(error));
